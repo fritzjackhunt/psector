6f5e46ec93b352bffa77784598ab72a582523b25
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-02-07 23:36:05 +0300

    Update

--- promosector/settings.py
+++ promosector/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xbzkh1lgk)v!#k270m6h&w_&)r$8wlt4-99r8zmxo6kyg(h+x6'
-
 with open('psector SK.txt') as f:
     SECRET_KEY = f.read().strip()
 
