5da396e32193f9a7208c98333bdf9be1d8d5e8d9
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-02-16 13:07:53 +0300

    pwa

--- promosector/urls.py
+++ promosector/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pwa.urls')),
     path('', include('project.urls')),
+    path('pwa', include('pwa.urls')),
 ]
