af41687beb22e53646dec88c84274adc90be5cef
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-02-16 13:18:14 +0300

    pwa

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='homepage'),
+    path('home', views.home, name='homepage'),
     path('aboutus', views.aboutus, name='aboutus'),
     path('news', views.news, name='news'),
     path('contacts', views.contacts, name='contacts'),
