71ce2fde31d597750a5a18db1fd9b6c09e5c9b17
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-02-19 15:22:44 +0300

    pwa

--- promosector/settings.py
+++ promosector/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath
 (__file__)))
 
 #PWA settings
-PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'promosector', 'static/js')
+#PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'promosector', 'static/js')
 
 
 # Quick-start development settings - unsuitable for production
